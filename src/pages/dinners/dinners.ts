c8d4781c6a25c046639c5d9afd58875f407358b3
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-02-27 20:49:43 +0100

    Added functionality for ingredientsmethod to work.

--- src/pages/dinners/dinners.ts
+++ src/pages/dinners/dinners.ts
@@ -23,7 +23,7 @@ export class DinnersPage {
   generateDinners() {
     this.chosenDinners = this.getSetAmountOfDinnersAtRandom(dinners, 7);
     this.dinnersHasBeenGenerated = true;
-    new File([JSON.stringify(this.chosenDinners)], "chosenDinners.json", {
+    new File([JSON.stringify(this.chosenDinners)], "../../assets/json/chosenDinners.json", {
       type: "text/json"
     });
     this.generateIngredientsJsonFileFromCurrentDinners();
@@ -48,27 +48,38 @@ export class DinnersPage {
 
     for (let i = 0; i < this.chosenDinners.length; i++) {
       let dinner: any = this.chosenDinners[i];
-
       for (let j = 0; j < dinner.ingredients.length; j++) {
         let ingredient: any = {
           ["itemName"]: dinner.ingredients[j].itemName,
           ["itemQuantity"]: dinner.ingredients[j].itemQuantity
         };
-        ingredients.push(ingredient);
-        this.addToIngredientInIngredientList(ingredients, ingredient);
+
+        if (this.addToIngredientInIngredientList(ingredients, ingredient)) {
+          (ingredients[j] as any).itemQuantity = +(ingredients[j] as any).itemQuantity + +ingredient.itemQuantity;
+        } else {
+          ingredients.push(ingredient);
+        }
       }
     }
+    new File([JSON.stringify(ingredients)],  "../../assets/json/grocerylist.json", {
+      type: "text/json"
+    });
   }
 
-  addToIngredientInIngredientList(ingredients: JSON[], ingredient: JSON) {
-    for(let i = 0; i < ingredients.length; i++) {
-      console.log("###########");
-      console.log(ingredients[i]);
-      console.log("###########");
+  addToIngredientInIngredientList(ingredients: JSON[], ingredient: any) {
+    for (let i = 0; i < ingredients.length; i++) {
+      if (ingredient.itemName === (ingredients[i] as any).itemName) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  sendIngredientsToGroceryList() {
+
   }
 
   goToDinnerDetailPage(dinner: JSON) {
-    this.navCtrl.push("DinnerDetailPage", {});
+    this.navCtrl.push("DinnerDetailPage", { dinner });
   }
 }
