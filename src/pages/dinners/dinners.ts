0239b1b93e91f633c87ea464e3c87652103bb92d
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-02-27 18:50:30 +0100

    Added some functionality for getting ingredients

--- src/pages/dinners/dinners.ts
+++ src/pages/dinners/dinners.ts
@@ -11,23 +11,25 @@ export class DinnersPage {
   private dinnersHasBeenGenerated: boolean = false;
   private chosenDinners: JSON[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-
-
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   /**
    * Method for generating 7 dinners.
    * Mainly for getting logic up and running.
    * Will create a JSON file containing 7 dinners.
+   * TODO add constraints for how many dinners to create
+   * ? Popup when clicking that chooses?
    */
   generateDinners() {
     this.chosenDinners = this.getSetAmountOfDinnersAtRandom(dinners, 7);
-    this.dinnersHasBeenGenerated = true;  
-    new File([JSON.stringify(this.chosenDinners)], "chosenDinners.json", {type: "text/json"})
+    this.dinnersHasBeenGenerated = true;
+    new File([JSON.stringify(this.chosenDinners)], "chosenDinners.json", {
+      type: "text/json"
+    });
+    this.generateIngredientsJsonFileFromCurrentDinners();
   }
 
-  getSetAmountOfDinnersAtRandom(dinnerArray : JSON[], amount : number ) {
+  getSetAmountOfDinnersAtRandom(dinnerArray: JSON[], amount: number) {
     var result = new Array(amount),
       len = dinnerArray.length,
       taken = new Array(len);
@@ -41,11 +43,32 @@ export class DinnersPage {
     return result;
   }
 
-  goToDinnerDetailPage(dinner: JSON) {
-    this.navCtrl.push('DinnerDetailPage', {
-      dinner
-    });
+  generateIngredientsJsonFileFromCurrentDinners() {
+    let ingredients: JSON[] = [];
+
+    for (let i = 0; i < this.chosenDinners.length; i++) {
+      let dinner: any = this.chosenDinners[i];
+
+      for (let j = 0; j < dinner.ingredients.length; j++) {
+        let ingredient: any = {
+          ["itemName"]: dinner.ingredients[j].itemName,
+          ["itemQuantity"]: dinner.ingredients[j].itemQuantity
+        };
+        ingredients.push(ingredient);
+        this.addToIngredientInIngredientList(ingredients, ingredient);
+      }
+    }
   }
 
+  addToIngredientInIngredientList(ingredients: JSON[], ingredient: JSON) {
+    for(let i = 0; i < ingredients.length; i++) {
+      console.log("###########");
+      console.log(ingredients[i]);
+      console.log("###########");
+    }
+  }
 
+  goToDinnerDetailPage(dinner: JSON) {
+    this.navCtrl.push("DinnerDetailPage", {});
+  }
 }
